b7c89ca7cfd40278a8dcd719957ef6ed859b7b82
Panos Mitzias <pmitzias@gmail.com>
2018-06-27 12:23:46 +0300

    minor changes

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='PyLOD',
+    packages=['PyLOD'],
+    version='0.1.0',
+    description='PyLOD is a Python wrapper for exposing Linked Open Data from public SPARQL-served endpoints.',
+    author='Panos Mitzias',
+    author_email='pmitzias@gmail.com',
+    url='https://github.com/panmitz/PyLOD',
+    download_url='https://github.com/panmitz/PyLOD/archive/0.1.0.tar.gz',
+    keywords=['0.1.0'],
+    classifiers=[],
+    install_requires=['sparqlwrapper']
+)
+
